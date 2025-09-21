e2e04ef695118b38bd1ffcc5281eee4db29a5106
Tirzaan Neil <Tirzaan@tntneil.com>
2025-09-20 21:20:49 -0600

    Initial Commit

---
+++ SwiftfulFirebase/SwiftfulFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseApp.swift
+//  SwiftfulFirebase
+//
+//  Created by Tirzaan on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
