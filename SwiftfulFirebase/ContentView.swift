327a5e3226ce2b126f2f180117c5f8e0ee28ef40
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-18 19:32:33 -0600

    [Test] Firebase Async Image

--- SwiftfulFirebase/ContentView.swift
+++ SwiftfulFirebase/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/testproject-d8a9e.firebasestorage.app/o/ChatGPT%20Image%20Oct%2017%2C%202025%20at%2010_35_46%20PM.png?alt=media&token=e2f3785f-6f44-47ab-93b3-0bc689bc05e6")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            AsyncImage(url: url, scale: 4)
+
             Text("Hello, world!")
         }
         .padding()
