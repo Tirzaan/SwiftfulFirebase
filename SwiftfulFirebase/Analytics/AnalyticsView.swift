c2ae0ab302bb3b4a693f86bbd66446aceafad30b
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-21 08:40:18 -0600

    [Comment] Analytics Code

--- SwiftfulFirebase/Analytics/AnalyticsView.swift
+++ SwiftfulFirebase/Analytics/AnalyticsView.swift
@@ -6,10 +6,49 @@
 //
 
 import SwiftUI
+import FirebaseAnalytics
+
+@MainActor
+final class AnalyticsManager {
+    static var shared = AnalyticsManager()
+    private init() { }
+    
+    func logEvent(name: String, parameters: [String : Any]? = nil) {
+        Analytics.logEvent(name, parameters: parameters)
+    }
+    
+    func setUserId(UserId: String) {
+        Analytics.setUserID(UserId)
+    }
+    
+    func SetUserProperty(value: String?, property: String) {
+        Analytics.setUserProperty(value, forName: property)
+    }
+}
 
 struct AnalyticsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Button("Log Event") {
+                AnalyticsManager.shared.logEvent(name: "AnalyticsView_LogEvent_ButtonClick")
+            }
+            
+            Button("Log Second Event") {
+                AnalyticsManager.shared.logEvent(name: "AnalyticsView_LogSecondEvent_ButtonClick", parameters: [
+                    "screen_title" : "Hello, world!"
+                ])
+            }
+        }
+        .analyticsScreen(name: "AnalyticsView")
+        .onAppear {
+            AnalyticsManager.shared.logEvent(name: "AnalyticsView_Appear")
+            
+            AnalyticsManager.shared.setUserId(UserId: "ABC123")
+            AnalyticsManager.shared.SetUserProperty(value: true.description.uppercased(), property: "user_is_premium")
+        }
+        .onDisappear {
+            AnalyticsManager.shared.logEvent(name: "AnalyticsView_Disappear")
+        }
     }
 }
 
--- SwiftfulFirebase/SwiftfulFirebaseApp.swift
+++ SwiftfulFirebase/SwiftfulFirebaseApp.swift
@@ -15,7 +15,7 @@ struct SwiftfulFirebaseApp: App {
     
     var body: some Scene {
         WindowGroup {
-            PreformanceView()
+            AnalyticsView()
         }
     }
 }
