a8548ace2635095892e5721bdb14df657e3e1849
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-21 08:39:40 -0600

    [Episode] 21 - Analytics

---
+++ SwiftfulFirebase/Analytics/AnalyticsView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalyticsView.swift
+//  SwiftfulFirebase
+//
+//  Created by Tirzaan on 10/20/25.
+//
+
+import SwiftUI
+
+struct AnalyticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalyticsView()
+}
